bfd3e942a992df33f69b9edddbce5465ba78c5d0
cherylhaz <cheryl.hazenberg@gmail.com>
2020-05-07 09:56:50 +0000

    code working

--- trailsdisplay.js
+++ trailsdisplay.js
@@ -1,4 +1,4 @@
 
     var countryLat = [{'name': 'ireland', 'lat': '53.1424'},{'name': 'italy', 'lat': '41.8719'}];
-    var selectedCountryLat = $.grep(countryLat, function(e){ return e.name == 'ireland'; });
-    console.log(selectedCountryLat);
+    var selectedCountryLat = countryLat.find(selectedCountryLat => selectedCountryLat.name === 'ireland');
+    console.log(selectedCountryLat.lat);
